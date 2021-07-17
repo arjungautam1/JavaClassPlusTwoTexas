2b4cd98e2ef89fd51699c961d8547c883a565ec9
arjungautam8877 <arjungautam8877@gmail.com>
2021-07-17 20:53:57 +0545

    array

---
+++ src/array/ArrayDemo.java
@@ -0,0 +1,26 @@
+package array;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        //declare array
+//        int[] numbers=new int[5];
+//        int[] numbers={10,30,20,40,12};
+        //initialize array
+//        numbers[0]=10;
+//        numbers[1]=20;
+//        numbers[3]=30;
+//
+//        for (int i=0;i<numbers.length;i++){
+//            System.out.println(numbers[i]);
+//        }
+
+//        for (int number:numbers) {
+//            System.out.println(number);
+//        }
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars.length);
+    }
+
+
+}
--- src/assignment/VowelConsonantMethod2.java
+++ src/assignment/VowelConsonantMethod2.java
@@ -14,14 +14,20 @@ public class VowelConsonantMethod2 {
     public static void main(String[] args) {
 
         for (;;) {
-            System.out.println("Enter any character(a-z)");
+
+            System.out.println("Enter any alphabet(a-z)");
             Scanner scanner = new Scanner(System.in);
             char input = scanner.next().charAt(0);
-            if (input == 'a' || input == 'e' || input == 'o' || input == 'i' || input == 'u') {
-                System.out.println("Vowel");
-            } else {
-                System.out.println("Consonant");
+
+            if((input>='a' && input<='z' || (input>='A'&& input<='Z'))) {
+                if (input == 'a' || input == 'e' || input == 'o' || input == 'i' || input == 'u') {
+                    System.out.println("Vowel");
+                } else {
+                    System.out.println("Consonant");
+                }
             }
+            else
+                System.out.println("Input is not alphabet");
         }
 
     }
