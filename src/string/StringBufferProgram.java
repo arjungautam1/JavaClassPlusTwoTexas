936aa4c7ed012a273bf9b623250d440a37b7cfa0
arjungautam1 <arjungautam5431@gmail.com>
2021-08-08 22:15:56 +0545

    String Buffer

---
+++ src/string/StringBufferProgram.java
@@ -0,0 +1,9 @@
+package string;
+
+public class StringBufferProgram {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer("Hello, ");
+        sb.append("World!");
+        System.out.println(sb);
+    }
+}
